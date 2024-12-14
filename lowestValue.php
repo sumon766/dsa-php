d53065a8ce748b81304e38b9bdb613fda955fa41
MD Mursedul Islam Sumon <sumon766@gmail.com>
2024-12-14 16:20:39 +0600

    Find the lowest value algorightm

---
+++ lowestValue.php
@@ -0,0 +1,15 @@
+<?php
+
+    //Find the lowest value in an array
+
+    function lowestValue ($arr) 
+    {
+        $lowest = $arr[0];
+
+        for ()
+    }
+
+    $my_array = [7, 12, 9, 4, 11];
+    print_r(lowestValue($my_array));
+
+?>
