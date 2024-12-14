ce84020f633d1c83875855f5dbcacc5056324d7f
MD Mursedul Islam Sumon <sumon766@gmail.com>
2024-12-14 16:30:45 +0600

    Find the second highest value in an array

---
+++ secondHighest.php
@@ -0,0 +1,13 @@
+<?php
+
+    //Find the second highest value in an array
+
+    function secondHighest ($arr) 
+    {
+
+    }
+
+    $my_array = [8, 13, 21, 16, 87, 23, 34, 60];
+    echo "The second highest value is : " . secondHighest($my_array);
+
+?>
