52d660308a75e41056e68a473def07133c4b3954
MD Mursedul Islam Sumon <sumon766@gmail.com>
2024-12-14 16:08:34 +0600

    Fibonacci number algorithm complete

--- fibonacci.php
+++ fibonacci.php
@@ -5,8 +5,15 @@
     $prev2 = 0;
     $prev1 = 1;
 
+    echo $prev2 . "<br>";
+    echo $prev1 . "<br>";
+
     for ($i = 0; $i < 18; $i++) {
-        
+        $newFibo = $prev1 + $prev2;
+        echo $newFibo . "<br>";
+
+        $prev2 = $prev1;
+        $prev1 = $newFibo;
     }
 
 ?>
